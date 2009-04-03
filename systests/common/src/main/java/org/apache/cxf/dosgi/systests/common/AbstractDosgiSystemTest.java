b1cb28d200692851e03c06fd4af46d421bb0bb65
A. J. David Bosschaert <davidb@apache.org>
2009-04-03 11:15:51 +0000

    Changed system tests to use Felix 1.4.1 instead of Equinox 3.5M4
    Removed the checked-in Equinox jar.
    Currently using Spring-DM 1.2.0-RC2-SNAPSHOT. Spring DM 1.2.0 is due out very soon, so we should be able to get rid of the 'SNAPSHOT' over the next couple of days. 
    Once Equinox 3.5 is released we should consider running the system tests under Equinox as well.
    Updated the Spring version from 2.5.5 to 2.5.6 (was needed for Spring DM 1.2.0)
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/dosgi/trunk@761619 13f79535-47bb-0310-9956-ffa450edef68

--- systests/common/src/main/java/org/apache/cxf/dosgi/systests/common/AbstractDosgiSystemTest.java
+++ systests/common/src/main/java/org/apache/cxf/dosgi/systests/common/AbstractDosgiSystemTest.java
@@ -19,17 +19,97 @@
 package org.apache.cxf.dosgi.systests.common;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.jar.Manifest;
 
-import org.apache.servicemix.kernel.testing.support.AbstractIntegrationTest;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
 
-public abstract class AbstractDosgiSystemTest extends AbstractIntegrationTest {
+public abstract class AbstractDosgiSystemTest extends AbstractConfigurableBundleCreatorTests {
+    private Properties dependencies;
+
+    // -------------------------------------------------------------------
+    // These methods are copied from the SMX Kernel Testing component. They
+    // are copied from the 
+    // org.apache.servicemix.kernel.testing.support.AbstractIntegrationTest 
+    // class. SMX Kernel 1.0 doesn't seem to work with Spring-DM 1.2, so 
+    // thats why these methods are duplicated here for the moment...
+    // -------------------------------------------------------------------
+    protected String getBundle(String groupId, String artifactId) {
+        return groupId + "," + artifactId + "," + getBundleVersion(groupId, artifactId);
+    }
+
+    protected String getBundleVersion(String groupId, String artifactId) {
+        if (dependencies == null) {
+            try {
+                File f = new File(System.getProperty("basedir"), "target/classes/META-INF/maven/dependencies.properties");
+                Properties prop = new Properties();
+                prop.load(new FileInputStream(f));
+                dependencies = prop;
+            } catch (IOException e) {
+                throw new IllegalStateException("Unable to load dependencies informations", e);
+            }
+        }
+        String version = dependencies.getProperty(groupId + "/" + artifactId + "/version");
+        if (version == null) {
+            throw new IllegalStateException("Unable to find dependency information for: " + groupId + "/" + artifactId + "/version");
+        }
+        return version;
+    }
+
+    protected File localMavenBundle(String groupId, String artifact, String version, String classifier, String type) {
+        String defaultHome = new File(new File(System.getProperty("user.home")), ".m2/repository").getAbsolutePath();
+        File repositoryHome = new File(System.getProperty("localRepository", defaultHome));
+
+        StringBuffer location = new StringBuffer(groupId.replace('.', '/'));
+        location.append('/');
+        location.append(artifact);
+        location.append('/');
+        location.append(getSnapshot(version));
+        location.append('/');
+        location.append(artifact);
+        location.append('-');
+        location.append(version);
+        if (classifier != null) {
+            location.append('-');
+            location.append(classifier);
+        }
+        location.append(".");
+        location.append(type);
+
+        return new File(repositoryHome, location.toString());
+    }
+
+    protected static boolean isTimestamped(String version) {
+        return version.matches(".+-\\d\\d\\d\\d\\d\\d\\d\\d\\.\\d\\d\\d\\d\\d\\d-\\d+");
+    }
+
+    protected static String getSnapshot(String version) {
+        if (isTimestamped(version)) {
+            return version.substring(0, version.lastIndexOf('-', version.lastIndexOf('-') - 1)) + "-SNAPSHOT";
+        }
+        return version;
+    }
+
+    protected void installBundle(String groupId, String artifactId, String classifier, String type) throws Exception {
+        String version = getBundleVersion(groupId, artifactId);
+        File loc = localMavenBundle(groupId, artifactId, version, classifier, type);
+        Bundle bundle = bundleContext.installBundle(loc.toURI().toString());
+        bundle.start();
+    }    
+    // -------------------------------------------------------------------
+    // End of SMX Kernel methods.
+    // -------------------------------------------------------------------
+    
+
     protected Manifest getManifest() {
         // let the testing framework create/load the manifest
         Manifest mf = super.getManifest();
@@ -39,7 +119,14 @@ public abstract class AbstractDosgiSystemTest extends AbstractIntegrationTest {
 
         return mf;
     }       
-        
+
+    @Override
+    protected String[] getTestFrameworkBundlesNames() {
+        List<String> names = new ArrayList<String>(Arrays.asList(super.getTestFrameworkBundlesNames()));
+        names.add(getBundle("org.apache.felix", "org.osgi.compendium"));
+        return names.toArray(new String [names.size()]);
+    }
+
     @Override
     protected Resource[] getTestBundles() {
         // Return the bundles for the current distribution, filtering out the 
