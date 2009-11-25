455b2d8113d71a5bfd1f199367935c290398ca3d
A. J. David Bosschaert <davidb@apache.org>
2009-11-25 10:46:39 +0000

    Merging the discovery single bundle distro into the pre-existing single bundle distro.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/dosgi/trunk@884025 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/single-bundle/src/test/java/org/apache/cxf/dosgi/singlebundle/AggregatedActivatorTest.java
+++ distribution/single-bundle/src/test/java/org/apache/cxf/dosgi/singlebundle/AggregatedActivatorTest.java
@@ -41,9 +41,12 @@ public class AggregatedActivatorTest extends TestCase {
 
     public void testReadResourcesFile() throws Exception {        
         String[] expected = {
+            "org.apache.felix.cm.impl.ConfigurationManager",
+            "org.apache.felix.fileinstall.FileInstall",
             "org.ops4j.pax.web.service.internal.Activator",
             "org.apache.cxf.dosgi.discovery.local.Activator",
             "org.apache.cxf.dosgi.dsw.Activator",
+            "org.apache.cxf.dosgi.discovery.zookeeper.Activator",
             "org.springframework.osgi.extender.internal.activator.ContextLoaderListener"};
         
         AggregatedActivator aa = new AggregatedActivator();
