a1006c6a3de327d3ea511a90b91ea5b65c31006f
Marc Schaaf <mschaaf@apache.org>
2010-06-25 12:47:32 +0000

    fixed the TopologyManagerImport test case
    git-svn-id: https://svn.apache.org/repos/asf/cxf/dosgi/trunk@957925 13f79535-47bb-0310-9956-ffa450edef68

--- dsw/cxf-topology-manager/src/test/java/org/apache/cxf/dosgi/topologymanager/TopologyManagerImportTest.java
+++ dsw/cxf-topology-manager/src/test/java/org/apache/cxf/dosgi/topologymanager/TopologyManagerImportTest.java
@@ -1,37 +1,53 @@
 package org.apache.cxf.dosgi.topologymanager;
 
 import java.util.Dictionary;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
+import org.easymock.IAnswer;
 import org.easymock.IMocksControl;
 import org.easymock.classextension.EasyMock;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.remoteserviceadmin.EndpointDescription;
+import org.osgi.service.remoteserviceadmin.ImportReference;
 import org.osgi.service.remoteserviceadmin.ImportRegistration;
 import org.osgi.service.remoteserviceadmin.RemoteServiceAdmin;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TopologyManagerImportTest {
 
     
     @Test
-    public void testImportForNewlyAddedRSA(){
+    public void testImportForNewlyAddedRSA() throws InterruptedException{
         
         IMocksControl c = EasyMock.createNiceControl();
 
+        c.makeThreadSafe(true);
+        
+        final Semaphore sema = new Semaphore(0);
+        
         BundleContext bc = c.createMock(BundleContext.class);
         ServiceRegistration sreg = c.createMock(ServiceRegistration.class);
         EasyMock.expect(bc.registerService((String)EasyMock.anyObject(), EasyMock.anyObject(), (Dictionary)EasyMock.anyObject())).andReturn(sreg).anyTimes();
-        
+         
         
         EndpointDescription epd = c.createMock(EndpointDescription.class);
         RemoteServiceAdmin rsa  = c.createMock(RemoteServiceAdmin.class);
-        ImportRegistration ireg = c.createMock(ImportRegistration.class);
+        final ImportRegistration ireg = c.createMock(ImportRegistration.class);
+        ImportReference iref = c.createMock(ImportReference.class);
         
-        EasyMock.expect(rsa.importService(EasyMock.eq(epd))).andReturn(ireg).once();
+        EasyMock.expect(rsa.importService(EasyMock.eq(epd))).andAnswer(new IAnswer<ImportRegistration>( ) {
 
+            public ImportRegistration answer() throws Throwable {
+                sema.release();
+                return ireg;
+            }
+        }).once();
+        EasyMock.expect(ireg.getImportReference()).andReturn(iref).anyTimes();
+        EasyMock.expect(iref.getImportedEndpoint()).andReturn(epd).anyTimes();
         c.replay();
         
         
@@ -47,6 +63,9 @@ public class TopologyManagerImportTest {
         rsaList.add(rsa);
         
         tm.triggerExportImportForRemoteSericeAdmin(rsa);
+        
+        assertTrue(sema.tryAcquire(10, TimeUnit.SECONDS));
+        
         
         tm.stop();
         
