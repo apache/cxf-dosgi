6d6a18cdf65770bd52ffc05d48a00b6cce6566ce
J. Daniel Kulp <dkulp@apache.org>
2012-12-14 17:18:38 +0000

    Cleanup the greeter sample
    git-svn-id: https://svn.apache.org/repos/asf/cxf/dosgi/trunk@1421987 13f79535-47bb-0310-9956-ffa450edef68

--- samples/greeter/client/src/main/java/org/apache/cxf/dosgi/samples/greeter/client/GreeterDialog.java
+++ samples/greeter/client/src/main/java/org/apache/cxf/dosgi/samples/greeter/client/GreeterDialog.java
@@ -1,21 +1,21 @@
-/** 
-  * Licensed to the Apache Software Foundation (ASF) under one 
-  * or more contributor license agreements. See the NOTICE file 
-  * distributed with this work for additional information 
-  * regarding copyright ownership. The ASF licenses this file 
-  * to you under the Apache License, Version 2.0 (the 
-  * "License"); you may not use this file except in compliance 
-  * with the License. You may obtain a copy of the License at 
-  * 
-  * http://www.apache.org/licenses/LICENSE-2.0 
-  * 
-  * Unless required by applicable law or agreed to in writing, 
-  * software distributed under the License is distributed on an 
-  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
-  * KIND, either express or implied. See the License for the 
-  * specific language governing permissions and limitations 
-  * under the License. 
-  */
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.cxf.dosgi.samples.greeter.client;
 
 import java.awt.Component;
@@ -42,6 +42,10 @@ import javax.swing.event.ChangeListener;
 public class GreeterDialog extends JDialog {
     private static final long serialVersionUID = 1L;
     
+    JTextField name1field;
+    JTextField name2field;
+    JTextField ageTextField;
+    JCheckBox throwExCB;
     Object selection;
 
     public GreeterDialog() {
@@ -56,42 +60,22 @@ public class GreeterDialog extends JDialog {
         rb1.setAlignmentX(Component.LEFT_ALIGNMENT);        
         panel.add(rb1);
         
-        final JPanel simplePanel = new JPanel(new GridBagLayout());
-        simplePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        GridBagConstraints c1 = new GridBagConstraints();
-        
+        final JPanel simplePanel = createFirstOptionPanel();
         rb1.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 enablePanel(simplePanel, rb1.isSelected());
             }
         });
-        
-        JLabel lb1 = new JLabel("Name: ");
-        c1.weightx = 0.0;
-        c1.gridx = 0;
-        c1.gridy = 0;
-        c1.insets = new Insets(0, 25, 0, 0);
-        c1.anchor = GridBagConstraints.LINE_START;
-        simplePanel.add(lb1, c1);
-        
-        final JTextField tf1 = new JTextField(20);
-        c1.weightx = 0.2;
-        c1.gridx = 1;
-        c1.gridy = 0;
-        c1.insets = new Insets(0, 10, 0, 0);
-        c1.anchor = GridBagConstraints.LINE_START;
-        simplePanel.add(tf1, c1);
         panel.add(simplePanel);
-
         panel.add(new JLabel(" ")); // add a spacer
         
-        final JRadioButton rb2 = new JRadioButton("invoke: GreetingPhrase [] greetMe(GreeterData data) throws GreeterException;");
+        final JRadioButton rb2 
+            = new JRadioButton("invoke: GreetingPhrase [] greetMe(GreeterData data) throws GreeterException;");
         rb2.setAlignmentX(Component.LEFT_ALIGNMENT);
         panel.add(rb2);
         
-        final JPanel complexPanel = new JPanel(new GridBagLayout());
-        complexPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        GridBagConstraints c2 = new GridBagConstraints();
+
+        final JPanel complexPanel = createSecondOptionPanel();
 
         rb2.addChangeListener(new ChangeListener() {            
             public void stateChanged(ChangeEvent e) {
@@ -99,47 +83,6 @@ public class GreeterDialog extends JDialog {
             }
         });            
         
-        JLabel lb2 = new JLabel("Name: ");
-        c2.weightx = 0.0;
-        c2.gridx = 0;
-        c2.gridy = 0;
-        c2.insets = new Insets(0, 25, 0, 0);
-        c2.anchor = GridBagConstraints.LINE_START;
-        complexPanel.add(lb2, c2);
-        
-        final JTextField tf2 = new JTextField(20);
-        c2.weightx = 0.2;
-        c2.gridx = 1;
-        c2.gridy = 0;
-        c2.insets = new Insets(0, 10, 0, 0);
-        c2.anchor = GridBagConstraints.LINE_START;
-        complexPanel.add(tf2, c2);        
-                
-        JLabel lb3 = new JLabel("Age: ");
-        c2.weightx = 0.0;
-        c2.gridx = 0;
-        c2.gridy = 1;
-        c2.insets = new Insets(0, 25, 0, 0);
-        c2.anchor = GridBagConstraints.LINE_START;
-        complexPanel.add(lb3, c2);
-        
-        final JTextField tf3 = new JTextField(7);
-        c2.weightx = 0.2;
-        c2.gridx = 1;
-        c2.gridy = 1;
-        c2.insets = new Insets(0, 10, 0, 0);
-        c2.anchor = GridBagConstraints.LINE_START;
-        complexPanel.add(tf3, c2);
-        
-        final JCheckBox cb1 = new JCheckBox("Throw Exception");
-        c2.weightx = 0.0;
-        c2.gridx = 0;
-        c2.gridy = 2;
-        c2.gridwidth = 2;
-        c2.insets = new Insets(0, 22, 0, 0);
-        c2.anchor = GridBagConstraints.LINE_START;
-        complexPanel.add(cb1, c2);
-
         panel.add(complexPanel);
         enablePanel(complexPanel, false);
         
@@ -152,9 +95,11 @@ public class GreeterDialog extends JDialog {
         b1.addActionListener(new ActionListener() {            
             public void actionPerformed(ActionEvent e) {
                 if (rb1.isSelected()) {
-                    selection = tf1.getText();
+                    selection = name1field.getText();
                 } else {
-                    selection = new GreeterDataImpl(tf2.getText(), new Integer(tf3.getText()), cb1.isSelected());
+                    selection = new GreeterDataImpl(name2field.getText(),
+                                                    new Integer(ageTextField.getText()), 
+                                                    throwExCB.isSelected());
                 }                
                 
                 setVisible(false);
@@ -170,6 +115,77 @@ public class GreeterDialog extends JDialog {
         pack();
         setLocationRelativeTo(null); // centers frame on screen
     }
+    
+    private JPanel createFirstOptionPanel() {
+        final JPanel simplePanel = new JPanel(new GridBagLayout());
+        simplePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        GridBagConstraints c1 = new GridBagConstraints();
+        
+        
+        JLabel lb1 = new JLabel("Name: ");
+        c1.weightx = 0.0;
+        c1.gridx = 0;
+        c1.gridy = 0;
+        c1.insets = new Insets(0, 25, 0, 0);
+        c1.anchor = GridBagConstraints.LINE_START;
+        simplePanel.add(lb1, c1);
+        
+        name1field = new JTextField(20);
+        c1.weightx = 0.2;
+        c1.gridx = 1;
+        c1.gridy = 0;
+        c1.insets = new Insets(0, 10, 0, 0);
+        c1.anchor = GridBagConstraints.LINE_START;
+        simplePanel.add(name1field, c1);
+        return simplePanel;
+    }
+    private JPanel createSecondOptionPanel() {
+        final JPanel complexPanel = new JPanel(new GridBagLayout());
+        complexPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        GridBagConstraints c2 = new GridBagConstraints();
+       
+        JLabel lb2 = new JLabel("Name: ");
+        c2.weightx = 0.0;
+        c2.gridx = 0;
+        c2.gridy = 0;
+        c2.insets = new Insets(0, 25, 0, 0);
+        c2.anchor = GridBagConstraints.LINE_START;
+        complexPanel.add(lb2, c2);
+        
+        name2field = new JTextField(20);
+        c2.weightx = 0.2;
+        c2.gridx = 1;
+        c2.gridy = 0;
+        c2.insets = new Insets(0, 10, 0, 0);
+        c2.anchor = GridBagConstraints.LINE_START;
+        complexPanel.add(name2field, c2);        
+                
+        JLabel lb3 = new JLabel("Age: ");
+        c2.weightx = 0.0;
+        c2.gridx = 0;
+        c2.gridy = 1;
+        c2.insets = new Insets(0, 25, 0, 0);
+        c2.anchor = GridBagConstraints.LINE_START;
+        complexPanel.add(lb3, c2);
+        
+        ageTextField = new JTextField(7);
+        c2.weightx = 0.2;
+        c2.gridx = 1;
+        c2.gridy = 1;
+        c2.insets = new Insets(0, 10, 0, 0);
+        c2.anchor = GridBagConstraints.LINE_START;
+        complexPanel.add(ageTextField, c2);
+        
+        throwExCB = new JCheckBox("Throw Exception");
+        c2.weightx = 0.0;
+        c2.gridx = 0;
+        c2.gridy = 2;
+        c2.gridwidth = 2;
+        c2.insets = new Insets(0, 22, 0, 0);
+        c2.anchor = GridBagConstraints.LINE_START;
+        complexPanel.add(throwExCB, c2);
+        return complexPanel;
+    }    
     
     public Object getSelection() {
         return selection;
