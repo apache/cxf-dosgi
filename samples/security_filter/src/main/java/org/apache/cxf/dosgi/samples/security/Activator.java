141c4246ff8d71554052b03f96feb1867dd229b3
J. Daniel Kulp <dkulp@apache.org>
2012-12-14 17:17:02 +0000

    Cleanup security_filter sample
    git-svn-id: https://svn.apache.org/repos/asf/cxf/dosgi/trunk@1421982 13f79535-47bb-0310-9956-ffa450edef68

--- samples/security_filter/src/main/java/org/apache/cxf/dosgi/samples/security/Activator.java
+++ samples/security_filter/src/main/java/org/apache/cxf/dosgi/samples/security/Activator.java
@@ -1,20 +1,20 @@
-/** 
- * Licensed to the Apache Software Foundation (ASF) under one 
- * or more contributor license agreements. See the NOTICE file 
- * distributed with this work for additional information 
- * regarding copyright ownership. The ASF licenses this file 
- * to you under the Apache License, Version 2.0 (the 
- * "License"); you may not use this file except in compliance 
- * with the License. You may obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
- * KIND, either express or implied. See the License for the 
- * specific language governing permissions and limitations 
- * under the License. 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.cxf.dosgi.samples.security;
 
@@ -28,32 +28,37 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 /**
- * Registers a REST endpoint and a servlet filter to control access to the endpoint.
+ * Registers a REST endpoint and a servlet filter to control access to the
+ * endpoint.
  */
 public class Activator implements BundleActivator {
-  private ServiceRegistration restRegistration;
-  private ServiceRegistration filterRegistration;
-  
-  @SuppressWarnings("unchecked")
-  public void start(BundleContext bundleContext) throws Exception {
-    // Register a rest endpoint
-    Dictionary restProps = new Hashtable();
-    restProps.put("service.exported.interfaces", SecureRestEndpoint.class.getName());
-    restProps.put("service.exported.configs", "org.apache.cxf.rs");
-    restProps.put("org.apache.cxf.rs.httpservice.context", "/secure");
-    restRegistration = bundleContext.registerService(SecureRestEndpoint.class.getName(), new SecureRestEndpoint(), restProps);
-    
-    // Register a servlet filter (this could be done in another OSGi bundle, too)
-    Dictionary filterProps = new Hashtable();
-    filterProps.put("org.apache.cxf.httpservice.filter", Boolean.TRUE);
-    // Pax-Web whiteboard (if deployed) will attempt to apply this filter to servlets by name or URL, and will complain
-    // if neither servletName or urlPatterns are specified.  The felix http service whiteboard may do something similar.
-    filterProps.put("servletNames", "none");
-    filterRegistration = bundleContext.registerService(Filter.class.getName(), new SampleSecurityFilter(), filterProps);
-  }
-  
-  public void stop(BundleContext bundleContext) throws Exception {
-    restRegistration.unregister();
-    filterRegistration.unregister();
-  }
+    private ServiceRegistration restRegistration;
+    private ServiceRegistration filterRegistration;
+
+    public void start(BundleContext bundleContext) throws Exception {
+        // Register a rest endpoint
+        Dictionary<String, Object> restProps = new Hashtable<String, Object>();
+        restProps.put("service.exported.interfaces", SecureRestEndpoint.class.getName());
+        restProps.put("service.exported.configs", "org.apache.cxf.rs");
+        restProps.put("org.apache.cxf.rs.httpservice.context", "/secure");
+        restRegistration = bundleContext.registerService(SecureRestEndpoint.class.getName(),
+                                                         new SecureRestEndpoint(), restProps);
+
+        // Register a servlet filter (this could be done in another OSGi bundle,
+        // too)
+        Dictionary<String, Object> filterProps = new Hashtable<String, Object>();
+        filterProps.put("org.apache.cxf.httpservice.filter", Boolean.TRUE);
+        // Pax-Web whiteboard (if deployed) will attempt to apply this filter to
+        // servlets by name or URL, and will complain
+        // if neither servletName or urlPatterns are specified. The felix http
+        // service whiteboard may do something similar.
+        filterProps.put("servletNames", "none");
+        filterRegistration = bundleContext.registerService(Filter.class.getName(),
+                                                           new SampleSecurityFilter(), filterProps);
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+        restRegistration.unregister();
+        filterRegistration.unregister();
+    }
 }
--- samples/security_filter/src/main/java/org/apache/cxf/dosgi/samples/security/SampleSecurityFilter.java
+++ samples/security_filter/src/main/java/org/apache/cxf/dosgi/samples/security/SampleSecurityFilter.java
@@ -1,20 +1,20 @@
-/** 
- * Licensed to the Apache Software Foundation (ASF) under one 
- * or more contributor license agreements. See the NOTICE file 
- * distributed with this work for additional information 
- * regarding copyright ownership. The ASF licenses this file 
- * to you under the Apache License, Version 2.0 (the 
- * "License"); you may not use this file except in compliance 
- * with the License. You may obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
- * KIND, either express or implied. See the License for the 
- * specific language governing permissions and limitations 
- * under the License. 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.cxf.dosgi.samples.security;
 
@@ -33,27 +33,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A filter that requires a query string of "secure" to invoke the protected resource.
+ * A filter that requires a query string of "secure" to invoke the protected
+ * resource.
  */
 public class SampleSecurityFilter implements Filter {
-  private static final Logger LOG = LoggerFactory.getLogger(SampleSecurityFilter.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SampleSecurityFilter.class);
 
-  public void destroy() {
-    LOG.info("destroy()");
-  }
-
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-          ServletException {
-    if("secure".equals(((HttpServletRequest)request).getQueryString())) {
-      LOG.info("Access granted");
-      chain.doFilter(request, response);
-    } else {
-      LOG.warn("Access denied");
-      ((HttpServletResponse)response).sendError(HttpServletResponse.SC_FORBIDDEN);
+    public void destroy() {
+        LOG.info("destroy()");
     }
-  }
 
-  public void init(FilterConfig config) throws ServletException {
-    LOG.info("init()");
-  }
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        if ("secure".equals(((HttpServletRequest)request).getQueryString())) {
+            LOG.info("Access granted");
+            chain.doFilter(request, response);
+        } else {
+            LOG.warn("Access denied");
+            ((HttpServletResponse)response).sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+        LOG.info("init()");
+    }
 }
